8002e676e56074a422ccff69df750d89ee4a9a2c
rus1anko <up.ruslanko@gmail.com>
2022-01-31 15:02:02 +0200

    pl

---
+++ lib/splash_screen.dart
@@ -0,0 +1,41 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_svg_animations_with_rive_tutorial/music_player_page.dart';
+import 'package:rive/rive.dart';
+
+class SplashScreen extends StatefulWidget {
+  const SplashScreen({Key? key}) : super(key: key);
+
+  @override
+  _SplashScreenState createState() => _SplashScreenState();
+}
+
+class _SplashScreenState extends State<SplashScreen> {
+  @override
+  void initState() {
+    super.initState();
+    Future.delayed(
+      Duration(seconds: 3),
+      () => Navigator.pushReplacement(
+        context,
+        MaterialPageRoute(
+          builder: (_) => MusicPlayerPage(),
+        ),
+      ),
+    );
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      backgroundColor: Colors.blue[600],
+      body: Center(
+        child: Container(
+          width: 400,
+          child: RiveAnimation.asset(
+            'assets/radioSplashAnimation.riv',
+          ),
+        ),
+      ),
+    );
+  }
+}
